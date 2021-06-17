31aff458a12afc9b293288cc080e138ebc66bd9a
FrancoCaneti <CAnetifranco95@hmail.com>
2021-06-17 12:46:57 +0200

    ok

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+   <div class="container">
+        <h1>I nostri POST</h1> 
+
+        <table class="table">
+          <thead>
+            <tr>
+               <th>Id</th>
+               <td>Title</td>
+               <th colspan='3'>Actions</th>
+            </tr>
+          </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                <tr>
+                   <td>{{ $post->id }}</td>
+                   <td>{{ $post->title }}</td>
+                   <td>SHOW</td>
+                   <td>EDIT</td>
+                   <td>DELETE</td>
+                </tr>
+            </tbody>
+        </table>  
+   </div>
+@endsection   
